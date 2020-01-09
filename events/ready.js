7dd87bcbc9526f63ba541f32307efd398d3b9b0f
maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2020-01-10 00:40:27 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,21 +4,21 @@ const mongoose = require('mongoose')
 
 module.exports = async bot => {
 console.log(`'${bot.user.username}' is online on ${bot.guilds.size} servers!`);
-// const status = [
-//   "Yumemiru Shoujo",
-//   "Chilled Lounge and Whatnot",
-//   "Changing colors",
-//   "SeishunButaBots.org"
-// ]
-// const types = [
-//   "PLAYING", "WATCHING", "LISTENING"
-// ]
-//
-// setTimeout(()=>{
-//   bot.user.setActivity(status[Math.floor(Math.random()*(status.length-1))],{
-//     type: types[Math.floor(Math.random()*(types.length-1))]
-//   })
-// },30000)
+const status = [
+  "Yumemiru Shoujo",
+  "Chilled Lounge and Whatnot",
+  "Changing colors",
+  "SeishunButaBots.org"
+]
+const types = [
+  "PLAYING", "WATCHING", "LISTENING"
+]
+
+setTimeout(()=>{
+  bot.user.setActivity(status[Math.floor(Math.random()*(status.length-1))],{
+     type: types[Math.floor(Math.random()*(types.length-1))]
+   })
+ },30000)
 
 
 }
